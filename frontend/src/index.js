d941492f8185a171981fabf7b728e598e1c5c447
De Cerval Come <come.decerval@student.ecp.fr>
2017-06-12 13:50:36 +0200

    Fix(frontend): Add the theme for display with material-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import injectTapEventPlugin from "react-tap-event-plugin";
 import App from "./components/App";
 import app from "./reducers";
 import {loadVariables, loadValues} from "./actions";
-import injectTapEventPlugin from "react-tap-event-plugin";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-
+/*
+ We use redux-thunk to be able to dispatch asynchronous actions
+ */
 const store = createStore(
     app,
     applyMiddleware(thunk)
@@ -20,12 +23,15 @@ const store = createStore(
 const rootEl = document.getElementById("root");
 
 const render = () => ReactDOM.render(
-    <App
-        datas={store.getState()}
-        changeVariables={(variable) => store.dispatch({type: "CHANGEVARIABLE", selected: variable})}
-        loadVariables={() => store.dispatch(loadVariables())}
-        loadValues={() => store.dispatch(loadValues())}
-    />,
+    <MuiThemeProvider>
+        <App
+            datas={store.getState()}
+            changeVariables={(variable) => store.dispatch({type: "CHANGEVARIABLE", selected: variable})}
+            loadVariables={() => store.dispatch(loadVariables())}
+            loadValues={() => store.dispatch(loadValues())}
+        />
+    </MuiThemeProvider>
+    ,
     rootEl,
 );
 
